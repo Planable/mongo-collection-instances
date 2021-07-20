b7ad4d808c512729d468340837c033e7c07d7150
Nick <nick@planable.io>
2021-07-20 12:33:43 +0300

    ⬆️ Update dependancies

--- package.js
+++ package.js
@@ -1,24 +1,22 @@
 Package.describe({
-  name: 'dburles:mongo-collection-instances',
-  summary: 'Access your Mongo instances',
-  version: '0.3.5',
-  git: 'https://github.com/dburles/mongo-collection-instances.git'
+  name: "planable:mongo-collection-instances",
+  summary: "Access your Mongo instances",
+  version: "0.3.6",
+  git: "https://github.com/Planable/mongo-collection-instances.git",
 });
 
-Package.onUse(function(api) {
-  api.versionsFrom('1.0');
-  api.use([
-    'mongo',
-    'underscore',
-    'lai:collection-extensions@0.2.1_1']);
-  api.addFiles('mongo-instances.js');
+Package.onUse(function (api) {
+  api.versionsFrom("2.3");
+  api.use(["mongo", "underscore", "planable:collection-extensions@0.2.3"]);
+  api.addFiles("mongo-instances.js");
 });
 
-Package.onTest(function(api) {
+Package.onTest(function (api) {
   api.use([
-    'tinytest',
-    'accounts-base',
-    'mongo',
-    'dburles:mongo-collection-instances']);
-  api.addFiles('mongo-instances-tests.js');
+    "tinytest",
+    "accounts-base",
+    "mongo",
+    "planable:mongo-collection-instances",
+  ]);
+  api.addFiles("mongo-instances-tests.js");
 });
